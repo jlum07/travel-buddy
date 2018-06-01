15c4ca2968be9ab809247e3ed9ffc16bcd8dfeff
Justin <j.lum07@gmail.com>
2018-05-31 22:40:40 -0400

    Cleaning up Create-React-App starter code.

---
+++ front-end/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a href="/" className="navbar-logo">Hello World, Travel Buddy</a>
+      </nav>
+    );
+  }
+}
+export default NavBar;
