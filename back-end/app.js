4cf06ccb79a64b66a03227497c97d1b63414430e
imack5 <iainsmackenzie@gmail.com>
2018-06-02 02:10:21 +0000

    Implemented API route for scraping the Trip Advisor website

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const tripAdvisor = require("./routes/tripAdvisor");
+const morgan      = require('morgan');
+
 // Set the port to 3001
 const PORT = 3001;
 
 // Create a new express server
-const server = express()
-  // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static("public"))
-  .listen(PORT, "0.0.0.0", "localhost", () =>
-    console.log(`Listening on ${PORT}`)
-  );
+const app = express();
+// Make the express app serve static assets (html, javascript, css) from the /public folder
+app.use(express.static("public"));
+
+app.use(morgan('dev'));
+
+app.use("/trip-advisor", tripAdvisor);
+
+app.listen(PORT, "0.0.0.0", "localhost", () =>
+  console.log(`Listening on ${PORT}`)
+);
