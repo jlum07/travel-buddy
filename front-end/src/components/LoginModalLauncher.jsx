fb75fa00bba563a5e64cdf50603e8524e0ef4c0c
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-06-03 14:31:06 -0400

    added login modal

---
+++ front-end/src/components/LoginModalLauncher.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import LoginForm from './LoginForm.jsx';
+import { Modal, Button  } from 'react-bootstrap';
+
+class LoginModalLauncher extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button bsStyle="primary" onClick={this.handleShow}>
+          Login
+        </Button>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <LoginForm handleClose={this.handleClose} />
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default LoginModalLauncher;
