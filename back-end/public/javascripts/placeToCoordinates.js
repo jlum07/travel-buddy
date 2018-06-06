0d6a6e20b952d0718865b41759ae7fb1d4da9a72
imack5 <iainsmackenzie@gmail.com>
2018-06-06 19:13:29 +0000

    Hardcoded in sample data to avoid excess API calls in development

--- back-end/public/javascripts/placeToCoordinates.js
+++ back-end/public/javascripts/placeToCoordinates.js
@@ -1,32 +1,45 @@
 const fetch = require("node-fetch");
-const apikey = require('../../apikey.js');
-const getSnapChats = require('./getSnapChats')
-const getInstagrams = require('./getInstagrams')
+const apikey = require("../../apikey.js");
+const getSnapChats = require("./getSnapChats");
+const getInstagrams = require("./getInstagrams");
 
 module.exports = function(placeElement) {
   return new Promise(function(resolve, reject) {
     fetch(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.6532,-79.3832&radius=15000&keyword=${placeElement.title}&key=${apikey.key}`
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.6532,-79.3832&radius=15000&keyword=${
+        placeElement.title
+      }&key=${apikey.key}`
     )
       .then(response => {
-        return response.json()
+        return response.json();
       })
       .then(async function(sup) {
         //console.log(sup)
-        placeElement.location = sup.results[0].geometry.location
+        placeElement.location = sup.results[0].geometry.location;
 
         //Adds the closest "address"
         //Could be improved by implenting a fetch to the google autocompete for places API
-        placeElement.address = sup.results[0].vicinity
-        console.log(placeElement.location.lat, placeElement.location.lng)
+        placeElement.address = sup.results[0].vicinity;
+        console.log(placeElement.location.lat, placeElement.location.lng);
 
-        placeElement.snapchat = await getSnapChats(placeElement.location.lat, placeElement.location.lng)
-        console.log("completed snap")
-        //placeElement.instagrams = await getInstagrams(placeElement.location.lat, placeElement.location.lng)
-        console.log("completed insta", )
+        placeElement.snapchat = await getSnapChats(
+          placeElement.location.lat,
+          placeElement.location.lng
+        );
+        console.log("completed snap");
+
+        // try {
+        //   placeElement.instagrams = await getInstagrams(
+        //     placeElement.location.lat,
+        //     placeElement.location.lng
+        //   );
+        // } catch (e) {
+        //   placeElement.instagrams = []
+        // }
+
+        console.log("completed insta");
 
         resolve(placeElement);
-      })
+      });
   });
 };
-
