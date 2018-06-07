0d9a838d381dc053de33d49889027a2869903e4f
imack5 <iainsmackenzie@gmail.com>
2018-06-07 17:21:28 +0000

    Refactored placeToCoordinates.js

--- back-end/public/javascripts/placeToCoordinates.js
+++ back-end/public/javascripts/placeToCoordinates.js
@@ -3,50 +3,47 @@ const apikey = require("./apikey.js");
 const getSnapChats = require("./getSnapChats");
 const getInstagrams = require("./getInstagrams");
 
-module.exports = function(placeElement, location) {
-  return new Promise(function(resolve, reject) {
-    console.log("here")
-    console.log(placeElement)
-    console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=15000&keyword=${
-        placeElement.title
-      }&key=${apikey.API_KEY}`)
-    fetch(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=15000&keyword=${
-        placeElement.title
-      }&key=${apikey.API_KEY}`
-    )
-      .then(async function(response){
-        return response.json();
-      })
-      .then(async function(sup) {
-        //console.log(sup)
-        //console.log(sup)
-        placeElement.location = sup.results[0].geometry.location;
-        placeElement.title = sup.results[0].name
-        //Adds the closest "address"
-        //Could be improved by implenting a fetch to the google autocompete for places API
-        placeElement.address = sup.results[0].vicinity;
-        console.log(placeElement.location.lat, placeElement.location.lng);
+module.exports = async function(placeElement, location) {
 
-        placeElement.snapchat = await getSnapChats(
-          placeElement.location.lat,
-          placeElement.location.lng
-        );
+  //Using Google Places API, search places by the place keyword from TA to return the most likely response
+  //and the coordinates/address
+  let response = await fetch(
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${
+      location.lat
+    },${location.lng}&radius=15000&keyword=${placeElement.title}&key=${
+      apikey.API_KEY
+    }`
+  );
 
-        console.log("completed snap");
+  //Convert to JSON
+  let resultJSON = await response.json();
 
-        // try {
-        //   placeElement.instagrams = await getInstagrams(
-        //     placeElement.location.lat,
-        //     placeElement.location.lng
-        //   );
-        // } catch (e) {
-        //   placeElement.instagrams = []
-        // }
+  //Add location data, plus full name
+  placeElement.location = resultJSON.results[0].geometry.location;
+  placeElement.title = resultJSON.results[0].name;
 
-        console.log("completed insta");
+  //Adds the closest "address"
+  //Could be improved by implenting a fetch to the google autocompete for places API
+  placeElement.address = resultJSON.results[0].vicinity;
 
-        resolve(placeElement);
-      });
-  });
+  //Get snapchats based on the place coordinates
+  placeElement.snapchat = await getSnapChats(
+    placeElement.location.lat,
+    placeElement.location.lng
+  );
+
+  console.log("completed snap");
+
+  // try {
+  //   placeElement.instagrams = await getInstagrams(
+  //     placeElement.location.lat,
+  //     placeElement.location.lng
+  //   );
+  // } catch (e) {
+  //   placeElement.instagrams = []
+  // }
+
+  console.log("completed insta");
+
+  return placeElement;
 };
