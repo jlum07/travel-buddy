87ad479f615570a49fdaf060c9c52aecab596b5f
imack5 <iainsmackenzie@gmail.com>
2018-06-07 01:27:59 +0000

    Started to increase the accuracy of the data returned and customized on a city by city basis

--- back-end/routes/tripAdvisor.js
+++ back-end/routes/tripAdvisor.js
@@ -4,27 +4,38 @@ const tripAdvisorScraper = require("../public/javascripts/tripAdvisorScraper");
 const placeToCoordinates = require("../public/javascripts/placeToCoordinates");
 const cityToPlaceCoordinates = require("../public/javascripts/cityToPlaceCoordinates");
 const getInstagrams = require("../public/javascripts/getInstagrams");
-
+const cityAutoComplete = require("../public/javascripts/cityAutoComplete");
 
 router.get("/", (req, res) => {
   tripAdvisorScraper(req.query.city).then(function(result) {
     res.send(JSON.stringify(result));
   });
 });
-
+//Barcelona = 41.402862, 2.174366
+//Toronto = lat: 43.6532, lng: -79.3832
 router.get("/urls", (req, res) => {
-  placeToCoordinates(req.query.place).then(function(result) {
+  console.log(req.query)
+  placeToCoordinates(req.query, {
+    lat: 41.402862,
+    lng: 2.174366
+  }).then(function(result) {
     res.send(result);
   });
 });
 
 router.get("/insta", async (req, res) => {
-    let result = await getInstagrams(43.6532, -79.3832)
-    res.send(result);
+  let result = await getInstagrams(43.6532, -79.3832);
+  res.send(result);
 });
 
 router.get("/test", (req, res) => {
   cityToPlaceCoordinates(req.query.city).then(function(result) {
+    res.send(result);
+  });
+});
+
+router.get("/auto", (req, res) => {
+  cityAutoComplete(req.query.city).then(function(result) {
     res.send(result);
   });
 });
