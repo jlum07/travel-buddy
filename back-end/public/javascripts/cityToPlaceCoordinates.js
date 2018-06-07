a81688bb002f8733b47acc67504896ced5b4264b
imack5 <iainsmackenzie@gmail.com>
2018-06-07 01:27:59 +0000

    Started to increase the accuracy of the data returned and customized on a city by city basis

--- back-end/public/javascripts/cityToPlaceCoordinates.js
+++ back-end/public/javascripts/cityToPlaceCoordinates.js
@@ -3,10 +3,12 @@ const placeToCoordinates = require("./placeToCoordinates");
 
 module.exports = function(searchInput) {
   return new Promise(function(resolve, reject) {
-    tripAdvisorScraper(searchInput)
+    //Pass the city name into it. The only reason of the full city details is for the location
+    tripAdvisorScraper(searchInput.address_components[0].long_name)
       .then(places => {
         let promiseArray = places.map(element => {
-          return placeToCoordinates(element);
+          //Pass the name of the attraction, and the location of the city to bias the results
+          return placeToCoordinates(element, searchInput.geometry.location);
         });
 
         return Promise.all(promiseArray).then(results => {
--- back-end/public/javascripts/placeToCoordinates.js
+++ back-end/public/javascripts/placeToCoordinates.js
@@ -1,22 +1,24 @@
 const fetch = require("node-fetch");
-const apikey = require("../../apikey.js");
+const apikey = require("./apikey.js");
 const getSnapChats = require("./getSnapChats");
 const getInstagrams = require("./getInstagrams");
 
-module.exports = function(placeElement) {
+module.exports = function(placeElement, location) {
   return new Promise(function(resolve, reject) {
+    console.log("here")
     fetch(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.6532,-79.3832&radius=15000&keyword=${
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=15000&keyword=${
         placeElement.title
-      }&key=${apikey.key}`
+      }&key=${apikey.API_KEY}`
     )
-      .then(response => {
+      .then(async function(response){
         return response.json();
       })
       .then(async function(sup) {
+        console.log(sup)
         //console.log(sup)
         placeElement.location = sup.results[0].geometry.location;
-
+        placeElement.title = sup.results[0].name
         //Adds the closest "address"
         //Could be improved by implenting a fetch to the google autocompete for places API
         placeElement.address = sup.results[0].vicinity;
