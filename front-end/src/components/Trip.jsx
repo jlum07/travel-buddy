e4a2d238bbb4d3a658c0ee17e778f4423890ef03
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-06-11 21:11:53 -0400

    users must now be logged in to view a specific trip. There is an error on the server when a user is viewing a trip, and logs out, but is appears to function correctly on the front end.

--- front-end/src/components/Trip.jsx
+++ front-end/src/components/Trip.jsx
@@ -29,13 +29,6 @@ class Trip extends React.Component {
     // console.log(timezone);
   }
 
-  // conponentDidMount(){
-  //   console.log('conponentDidMount');
-  // }
-  // conponentWillMount(){
-  //   console.log('conponentWillMount');
-  // }
-
   componentWillReceiveProps(newProps) {
     axios.get(`http://localhost:3001/trips/${newProps.match.params.id}`, {
       params: {
@@ -57,30 +50,26 @@ class Trip extends React.Component {
     .catch( error => {
       console.log("error: ", error);
     });
-
   }
 
 
   render(){
-    return (
-      <React.Fragment>
-        <TripMap itinerary={this.state.itinerary} />
-        <AddModal userId={this.props.currentUser.id} tripId={this.props.match.params.id} />
-        <TripCityList itinerary={this.state.itinerary} />
-        <TripEventTimeline itinerary={this.state.itinerary} />
-        <TripTimeline itinerary={this.state.itinerary} />
-      </React.Fragment>
-    );
+    if (this.props.currentUser.id === null){
+      return <h1>Please log in to view trip</h1>
+    }
+    else {      
+      return (
+        <React.Fragment>
+          <TripMap itinerary={this.state.itinerary} />
+          <AddModal userId={this.props.currentUser.id} tripId={this.props.match.params.id} />
+          <TripCityList itinerary={this.state.itinerary} />
+          <TripEventTimeline itinerary={this.state.itinerary} />
+          <TripTimeline itinerary={this.state.itinerary} />
+        </React.Fragment>
+      );
+    }
   }
 }
 
 export default Trip;
 
-// const cities = [
-//   {order: 0, city: "Paris", start_date: "2018-07-07", end_date: "2018-07-17", duration: 100, lat: 48.8567, lng: 2.3510},
-//   {order: 1, city: "Toronto", start_date: "2018-07-07", end_date: "2018-07-17",  duration: 100, lat: 43.8163, lng: -79.4287},
-//   {order: 2, city: "Los Angeles", start_date: "2018-07-07", end_date: "2018-07-17",  duration: 100, lat: 34.3, lng: -118.15},
-//   {order: 3, city: "Havana", start_date: "2018-07-07", end_date: "2018-07-17",  duration: 100, lat: 23, lng: -82},
-//   {order: 4, city: "Bogotá", start_date: "2018-07-07", end_date: "2018-07-17",  duration: 100, lat: 4.598056, lng: -74.075833},
-//   {order: 5, city: "Saint John", start_date: "2018-07-07", end_date: "2018-07-17",  duration: 100, lat: 45.2796, lng: -66.0628}
-// ]
