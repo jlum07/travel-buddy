241cc3d14bd98e5b5a010acb5161cd9c0895d842
imack5 <iainsmackenzie@gmail.com>
2018-06-12 17:50:39 +0000

    Added scrollable POI list that links to modal

--- front-end/src/components/DashboardComponents/Dropdown/Dropdown.jsx
+++ front-end/src/components/DashboardComponents/Dropdown/Dropdown.jsx
@@ -5,7 +5,7 @@ import "./Dropdown.css";
 class Dropdown extends React.Component {
   render() {
     return (
-      <DropdownButton title={this.props.currentCat.title} key={1} id={`sup`}>
+      <DropdownButton title={this.props.currentCat.title} key={1} id="dropdown">
         <MenuItem onSelect={this.props.handleClick} eventKey="top_poi">
           Popular
         </MenuItem>
